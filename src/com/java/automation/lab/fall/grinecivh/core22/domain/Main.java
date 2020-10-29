50cd75b4d88ae2dd877e3dd0e9e7bcf4dd887ad4
indasto <indasto1@mail.ru>
2020-10-29 21:00:56 +0300

    CORE6.2

--- src/com/java/automation/lab/fall/grinecivh/core22/domain/Main.java
+++ src/com/java/automation/lab/fall/grinecivh/core22/domain/Main.java
@@ -10,18 +10,18 @@ public class Main {
         AirlineCompany airlineCompany = new AirlineCompany("Test", "Test street");
 
         airlineCompany.getDictionary().addNewDictionary(DictionaryEntity.AIRPORT_LICENSE,"License",
-                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\CORE2.2\\src\\resources\\directory\\license.txt");
+                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\Airport\\src\\resources\\directory\\license.dat");
 
         airlineCompany.getDictionary().addInDictionary("\nLicence extend", DictionaryEntity.AIRPORT_LICENSE);
         airlineCompany.getDictionary().printDictionary(
-                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\CORE2.2\\src\\resources\\directory\\license.txt");
+                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\Airport\\src\\resources\\directory\\license.dat");
 
         airlineCompany.getDictionary().addNewDictionary(DictionaryEntity.FLIGHT_COMMENT, "5 stars" ,
-                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\CORE2.2\\src\\resources\\directory\\Flight com.txt");
+                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\Airport\\src\\resources\\directory\\Flight com.dat");
         airlineCompany.getDictionary().addNewDictionary(DictionaryEntity.LUGGAGE_RULE,"Rule 1\nRule 2\n\t2.1\n\t2.2",
-                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\CORE2.2\\src\\resources\\directory\\Luggage rule.txt");
+                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\Airport\\src\\resources\\directory\\Luggage rule.dat");
         airlineCompany.getDictionary().addNewDictionary(DictionaryEntity.AIRLINE_COMPANY_COMMENT,"Won't fly with them ever again",
-                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\CORE2.2\\src\\resources\\directory\\airline company com.txt");
+                "C:\\Users\\indasto\\IdeaProjects\\Solvd\\Airport\\src\\resources\\directory\\airline company com.dat");
 
 
     }
